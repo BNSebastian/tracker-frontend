a0979afbb33deb9bd14f4aaf811e20b143141048
BNSebastian <bucovala.sebastian@protonmail.com>
2023-12-04 01:08:21 +0200

    updated route

--- src/app/_environments/backend.ts
+++ src/app/_environments/backend.ts
@@ -11,5 +11,4 @@ backendUrl.register = `${backendUrl.home}/auth/register`;
 backendUrl.authenticate = `${backendUrl.home}/auth/authenticate`;
 backendUrl.activity = `${backendUrl.home}/activity`;
 backendUrl.type = `${backendUrl.home}/type`;
-
-backendUrl.getTime = `${backendUrl.activity}/time/month&type`;
+backendUrl.getTime = `${backendUrl.activity}/time`;
